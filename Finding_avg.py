0ae2e74a2a93d21af4e8bd684aafc0a9014b1103
Varun M <varun.m15112008@gmail.com>
2026-08-26 20:01:25 +0530

    Add average score calculation for queried student
    Calculate average score for a specific student based on input marks.

--- Finding_avg.py
+++ Finding_avg.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     
     for i in range(n):
         name, *line = input().split()
+        
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name=input()   
